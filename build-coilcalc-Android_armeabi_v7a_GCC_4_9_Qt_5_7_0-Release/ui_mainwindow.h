80975ed32d7a6af784ebf35366284bca1bea6c0e
Apanassenko Bogdan <brstudio92@gmail.com>
2016-11-17 12:26:57 +0300

    v2 10percent

--- build-coilcalc-Android_armeabi_v7a_GCC_4_9_Qt_5_7_0-Release/ui_mainwindow.h
+++ build-coilcalc-Android_armeabi_v7a_GCC_4_9_Qt_5_7_0-Release/ui_mainwindow.h
@@ -14,11 +14,14 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QFrame>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -38,6 +41,15 @@ public:
     QLabel *label_5;
     QLabel *label_6;
     QPushButton *pushButton_2;
+    QGroupBox *groupBox;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QRadioButton *radioButton_2;
+    QRadioButton *radioButton;
+    QRadioButton *radioButton_3;
+    QRadioButton *radioButton_4;
+    QRadioButton *radioButton_5;
+    QRadioButton *radioButton_6;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -45,6 +57,9 @@ public:
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
         MainWindow->setEnabled(true);
         MainWindow->resize(800, 480);
+        MainWindow->setAutoFillBackground(false);
+        MainWindow->setStyleSheet(QLatin1String("background-color: rgb(70, 70, 70);\n"
+"color: rgb(255, 255, 255);"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         pushButton = new QPushButton(centralWidget);
@@ -52,20 +67,20 @@ public:
         pushButton->setGeometry(QRect(90, 310, 80, 30));
         label = new QLabel(centralWidget);
         label->setObjectName(QStringLiteral("label"));
-        label->setGeometry(QRect(30, 65, 81, 30));
+        label->setGeometry(QRect(19, 115, 81, 30));
         label_2 = new QLabel(centralWidget);
         label_2->setObjectName(QStringLiteral("label_2"));
-        label_2->setGeometry(QRect(30, 109, 81, 20));
+        label_2->setGeometry(QRect(19, 159, 81, 20));
         lineEdit = new QLineEdit(centralWidget);
         lineEdit->setObjectName(QStringLiteral("lineEdit"));
-        lineEdit->setGeometry(QRect(120, 70, 113, 21));
+        lineEdit->setGeometry(QRect(109, 120, 113, 21));
         lineEdit_2 = new QLineEdit(centralWidget);
         lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
-        lineEdit_2->setGeometry(QRect(121, 110, 113, 21));
+        lineEdit_2->setGeometry(QRect(110, 160, 113, 21));
         frame = new QFrame(centralWidget);
         frame->setObjectName(QStringLiteral("frame"));
         frame->setEnabled(true);
-        frame->setGeometry(QRect(10, 160, 271, 111));
+        frame->setGeometry(QRect(10, 200, 271, 111));
         frame->setFrameShape(QFrame::NoFrame);
         frame->setFrameShadow(QFrame::Plain);
         label_4 = new QLabel(frame);
@@ -83,6 +98,47 @@ public:
         pushButton_2 = new QPushButton(centralWidget);
         pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
         pushButton_2->setGeometry(QRect(190, 320, 80, 21));
+        groupBox = new QGroupBox(centralWidget);
+        groupBox->setObjectName(QStringLiteral("groupBox"));
+        groupBox->setGeometry(QRect(10, 20, 261, 81));
+        horizontalLayoutWidget = new QWidget(groupBox);
+        horizontalLayoutWidget->setObjectName(QStringLiteral("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(0, 20, 261, 51));
+        horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        radioButton_2 = new QRadioButton(horizontalLayoutWidget);
+        radioButton_2->setObjectName(QStringLiteral("radioButton_2"));
+
+        horizontalLayout->addWidget(radioButton_2);
+
+        radioButton = new QRadioButton(horizontalLayoutWidget);
+        radioButton->setObjectName(QStringLiteral("radioButton"));
+
+        horizontalLayout->addWidget(radioButton);
+
+        radioButton_3 = new QRadioButton(horizontalLayoutWidget);
+        radioButton_3->setObjectName(QStringLiteral("radioButton_3"));
+
+        horizontalLayout->addWidget(radioButton_3);
+
+        radioButton_4 = new QRadioButton(horizontalLayoutWidget);
+        radioButton_4->setObjectName(QStringLiteral("radioButton_4"));
+
+        horizontalLayout->addWidget(radioButton_4);
+
+        radioButton_5 = new QRadioButton(horizontalLayoutWidget);
+        radioButton_5->setObjectName(QStringLiteral("radioButton_5"));
+
+        horizontalLayout->addWidget(radioButton_5);
+
+        radioButton_6 = new QRadioButton(horizontalLayoutWidget);
+        radioButton_6->setObjectName(QStringLiteral("radioButton_6"));
+
+        horizontalLayout->addWidget(radioButton_6);
+
         MainWindow->setCentralWidget(centralWidget);
 
         retranslateUi(MainWindow);
@@ -101,6 +157,13 @@ public:
         label_5->setText(QApplication::translate("MainWindow", "0", 0));
         label_6->setText(QApplication::translate("MainWindow", "0", 0));
         pushButton_2->setText(QApplication::translate("MainWindow", "PushButton", 0));
+        groupBox->setTitle(QApplication::translate("MainWindow", "\320\272\320\276\320\273\320\270\321\207\320\265\321\201\321\202\320\262\320\276 \320\277\321\200\320\276\320\262\320\276\320\264\320\276\320\262", 0));
+        radioButton_2->setText(QApplication::translate("MainWindow", "1", 0));
+        radioButton->setText(QApplication::translate("MainWindow", "2", 0));
+        radioButton_3->setText(QApplication::translate("MainWindow", "3", 0));
+        radioButton_4->setText(QApplication::translate("MainWindow", "4", 0));
+        radioButton_5->setText(QApplication::translate("MainWindow", "5", 0));
+        radioButton_6->setText(QApplication::translate("MainWindow", "6", 0));
     } // retranslateUi
 
 };
